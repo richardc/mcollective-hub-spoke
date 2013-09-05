78db8f219cb4491026c25dfc1776ea208dea0cea
Richard Clamp <richardc@unixbeard.net>
2013-09-05 16:07:15 +0100

    reduce some duplication
    this adds an evil name-based branch, but we're professional stunt people

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,29 +1,8 @@
-node 'hub.example.com' {
-  class { 'vagrant_hosts': } ->
-  class { 'puppetlabs_repos': } ->
-  package { 'hiera':
-    ensure => latest,
-  } ->
-  class { 'mcollective':
-    client             => true,
-    middleware         => true,
-    middleware_hosts   => [ 'localhost' ],
-    securityprovider   => 'psk',
-    activemq_template  => 'site_mcollective/activemq.xml.erb',
-    ssl_client_certs   => 'puppet:///modules/site_mcollective/certs',
-    ssl_ca_cert        => 'puppet:///modules/site_mcollective/certs/ca.pem',
-    ssl_server_public  => 'puppet:///modules/site_mcollective/certs/server.pem',
-    ssl_server_private => 'puppet:///modules/site_mcollective/private_keys/server.pem',
+node 'hub.example.com', /^broker\d+\.example.com/ {
+  if $hostname =~ /broker/ {
+    $mcollective_hub = 'hub.example.com'
   }
 
-  mcollective::user { 'vagrant':
-    certificate => 'puppet:///modules/site_mcollective/certs/root.pem',
-    private_key => 'puppet:///modules/site_mcollective/private_keys/root.pem',
-  }
-}
-
-node /^broker\d+\.example.com/ {
-  $mcollective_hub = 'hub.example.com'
   class { 'vagrant_hosts': } ->
   class { 'puppetlabs_repos': } ->
   package { 'hiera':
